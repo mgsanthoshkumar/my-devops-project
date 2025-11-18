ca8f47175af909d6ae04e50150efd00b5be4d870
MG Santhosh Kumar <santhoshkumarofficial7@gmail.com>
2025-11-18 17:00:33 +0530

    CHORE: Integrate Terraform IaC job into CI/CD pipeline

--- terraform-docker-project/main.tf
+++ terraform-docker-project/main.tf
@@ -1,4 +1,7 @@
+# main.tf
+
 # 1. Define the required provider (Docker)
+# This ensures Terraform knows how to interact with your local Docker environment.
 terraform {
   required_providers {
     docker = {
@@ -9,18 +12,21 @@ terraform {
 }
 
 # 2. Pull the Nginx Docker Image
+# This defines the base image for our web server.
 resource "docker_image" "nginx_image" {
   name = "nginx:latest"
 }
 
 # 3. Create and Run the Docker Container
+# This provisions the actual server instance.
 resource "docker_container" "nginx_container" {
   name  = "my-local-nginx-server"
   image = docker_image.nginx_image.name
   
-  # Map port 80 in the container to port 8080 on your local machine
+  # Use the variable defined in variables.tf for the external port.
+  # This makes the config reusable and easily changed without editing this file.
   ports {
     internal = 80
-    external = var.external_port
+    external = var.external_port 
   }
 }
